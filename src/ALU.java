d0995a0d7dbb931aa2ff054990fbe5f52c55c004
Night <jpaliprantis@gmail.com>
2021-10-21 17:21:26 -0400

    Added ALU with add/subtract operations

--- src/ALU.java
+++ src/ALU.java
@@ -5,11 +5,53 @@ public class ALU {
     private boolean carryoutFlag;
     private boolean overflowFlag;
 
+    public ALU() {
+        zeroFlag = false;
+        negativeFlag = false;
+        carryoutFlag = false;
+        overflowFlag = false;
+    }
 
-    private LongWord rippleCarryAdd(LongWord a, LongWord b, boolean cin) {
+    public LongWord operate(int code, LongWord op1, LongWord op2) {
         LongWord result = new LongWord();
 
+        return result;
+    }
 
+    private LongWord rippleCarryAdd(LongWord a, LongWord b, boolean cin) {
+        LongWord result;
+        if(!cin) {
+            for (int i = 0; i < 32; i++) {
+
+                LongWord carry;
+                //contains the bits that are common amongst a and b.
+                carry = a.and(b);
+
+                // a now only has bits set where only one of the bits in either a or b is set.
+                a = a.xor(b);
+
+                // b now become the result of shifting the carry by one.
+                b = carry.shiftLeftLogical(1);
+
+            }
+            result = a;
+            if(result.isZero()) {
+                zeroFlag = true;
+            }
+        }
+        else {
+            for(int i = 31; i > -1; i--){
+                LongWord added;
+                LongWord borrow;
+                // flip the bits of the minuend (a)
+                borrow = a.not();
+
+                added = borrow.and(b);
+                a = a.xor(b);
+                b = added.shiftLeftLogical(1);
+            }
+            result = a;
+        }
         return result;
     }
 
---
+++ src/TestALU.java
@@ -0,0 +1,12 @@
+public class TestALU {
+    public static void main(String[] args) {
+        ALU alu = new ALU();
+
+        LongWord l1 = new LongWord();
+        l1.set(245);
+        LongWord l2 = new LongWord();
+        l2.set(55);
+
+        //System.out.println(alu.rippleCarryAdd(l1, l2, true));
+    }
+}
